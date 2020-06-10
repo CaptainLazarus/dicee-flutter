946b18530bcee602b283fb8e7f94841e2442e5e8
MisterKnight <adityagudimetla@gmail.com>
2020-06-10 18:28:37 +0530

    Great

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+//import 'package:flutter/rendering.dart';
 
 void main() {
   return runApp(
@@ -9,15 +10,50 @@ void main() {
           title: Text('Dicee'),
           backgroundColor: Colors.red,
         ),
-        body: DicePage(),
+        body: DiceState(),
       ),
     ),
   );
 }
 
-class DicePage extends StatelessWidget {
+class DiceState extends StatefulWidget {
+  @override
+  _DiceStateState createState() => _DiceStateState();
+}
+
+class _DiceStateState extends State<DiceState> {
+
+  int d1 = 1;
+  int d2 = 1;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+      child: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Expanded(
+              flex: 1,
+              child: FlatButton(
+                onPressed: () {
+                  setState(() {
+                    d1 = 3;
+                  });
+                },
+                child: Image.asset('images/dice$d1.png'),
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: FlatButton(
+                onPressed: () => {},
+                child: Image.asset('images/dice$d2.png'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
